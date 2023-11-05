c31b8c615eba599e6760e5b0e2ee8fd67064ae23
betty2310 <huynh.dh2310@gmail.com>
2023-11-05 22:19:46 +0700

    feat: using goroutine to improve time execute

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/fatih/color"
@@ -46,7 +47,33 @@ func fetchMatches(date string) types.Events {
 	}
 	matchesFilePath := filepath.Join(DEFAULT_PATH, strings.Replace(date, "-", "_", -1), "matches.json")
 	writeJson(matches, matchesFilePath)
+	color.Green("✅ Successfully crawl matches on %s", date)
 	return matches
+}
+
+func fetchTable(date string) types.TableData {
+	color.Blue("Crawl tournament table on %s", date)
+	table, err := server.GetTable()
+	if err != nil {
+		log.Println(err)
+	}
+	name := strings.Replace(table.Standings[0].Name, "/", "-", -1) + "_table.json"
+	tableFilePath := filepath.Join(DEFAULT_PATH, strings.Replace(date, "-", "_", -1), name)
+	writeJson(table, tableFilePath)
+	color.Green("✅ Successfully crawl %s table!", table.Standings[0].Name)
+	return table
+}
+
+func fetchTournamentStatistic(date string, tournamentName string) {
+	color.Blue("Crawl tournament statistic on %s", date)
+	statistic, err := server.GetTournamentStatistic()
+	if err != nil {
+		log.Println(err)
+	}
+	name := strings.Replace(tournamentName, "/", "-", -1) + "_overall_statistic.json"
+	sFilePath := filepath.Join(DEFAULT_PATH, strings.Replace(date, "-", "_", -1), name)
+	writeJson(statistic, sFilePath)
+	color.Green("✅ Successfully crawl %s overall statistic!", tournamentName)
 }
 
 func main() {
@@ -59,77 +86,74 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	date := os.Getenv("DATE")
-	matches := fetchMatches(date)
-	color.Green("✅ Successfully crawl matches on %s", date)
+	tc := make(chan types.TableData)
+	mc := make(chan types.Events)
+	go func() {
+		tc <- fetchTable(date)
+	}()
+	go func() {
+		mc <- fetchMatches(date)
+	}()
+	table := <-tc
+	tournamentName := table.Standings[0].Name
+	fetchTournamentStatistic(date, tournamentName)
 
-	color.Blue("Crawl tournament table on %s", date)
-	table, err := server.GetTable()
-	if err != nil {
-		log.Println(err)
-	}
-	name := strings.Replace(table.Standings[0].Name, "/", "-", -1) + "_table.json"
-	tableFilePath := filepath.Join(DEFAULT_PATH, strings.Replace(date, "-", "_", -1), name)
-	writeJson(table, tableFilePath)
-	color.Green("✅ Successfully crawl %s table!", table.Standings[0].Name)
+	matches := <-mc
 
-	color.Blue("Crawl tournament statistic on %s", date)
-	statistic, err := server.GetTournamentStatistic()
-	if err != nil {
-		log.Println(err)
-	}
-	name = strings.Replace(table.Standings[0].Name, "/", "-", -1) + "_overall_statistic.json"
-	sFilePath := filepath.Join(DEFAULT_PATH, strings.Replace(date, "-", "_", -1), name)
-	writeJson(statistic, sFilePath)
-	color.Green("✅ Successfully crawl %s overall statistic!", table.Standings[0].Name)
-
+	wg := sync.WaitGroup{}
 	for _, event := range matches.Events {
 		if event.Tournament.Name == "Premier League" && event.Tournament.ID == 1 {
+			wg.Add(1)
 			filePath := filepath.Join(DEFAULT_PATH, strings.Replace(date, "-", "_", -1), "matches")
-			if event.Status.Type == "inprogress" {
-				filePath = filepath.Join(filePath, "inprogress")
-				// inprogress match statistic
-				fmt.Println("\tCrawl match", event.Slug, "statistic")
-				statistic, err := server.GetStatistic(event.ID)
+			go func(event types.Event) {
+				defer wg.Done()
+				if event.Status.Type == "inprogress" {
+					filePath = filepath.Join(filePath, "in_progress_matches")
+					// inprogress match statistic
+					color.Yellow("\tCrawl match %s statistic", event.Slug)
+					statistic, err := server.GetStatistic(event.ID)
+					if err != nil {
+						log.Println(err)
+					}
+					name := fmt.Sprint(event.ID) + "_statistic.json"
+					st := filepath.Join(filePath, event.Slug, name)
+					writeJson(statistic, st)
+				}
+				filePath = filepath.Join(filePath, event.Slug)
+
+				// lineup
+				color.Cyan("\tCrawl match %s lineup", event.Slug)
+				lineup, err := server.GetLineups(event.ID)
 				if err != nil {
 					log.Println(err)
 				}
-				name := fmt.Sprint(event.ID) + "_statistic.json"
-				st := filepath.Join(filePath, name)
-				writeJson(statistic, st)
-			}
-			filePath = filepath.Join(filePath, event.Slug)
+				name := fmt.Sprint(event.ID) + "_lineup.json"
+				lineupFilePath := filepath.Join(filePath, name)
+				writeJson(lineup, lineupFilePath)
 
-			// lineup
-			fmt.Println("\tCrawl match", event.Slug, "lineup")
-			lineup, err := server.GetLineups(event.ID)
-			if err != nil {
-				log.Println(err)
-			}
-			name := fmt.Sprint(event.ID) + "_lineup.json"
-			lineupFilePath := filepath.Join(filePath, name)
-			writeJson(lineup, lineupFilePath)
+				// h2h
+				fmt.Println("\tCrawl match", event.Slug, "h2h")
+				h2h, err := server.GetH2H(event.ID)
+				if err != nil {
+					log.Println(err)
+				}
+				name = fmt.Sprint(event.ID) + "_h2h.json"
+				h2hFilePath := filepath.Join(filePath, name)
+				writeJson(h2h, h2hFilePath)
 
-			// h2h
-			fmt.Println("\tCrawl match", event.Slug, "h2h")
-			h2h, err := server.GetH2H(event.ID)
-			if err != nil {
-				log.Println(err)
-			}
-			name = fmt.Sprint(event.ID) + "_h2h.json"
-			h2hFilePath := filepath.Join(filePath, name)
-			writeJson(h2h, h2hFilePath)
-
-			// pregame form
-			fmt.Println("\tCrawl match", event.Slug, "pregame form")
-			pre, err := server.GetPregameForm(event.ID)
-			if err != nil {
-				log.Println(err)
-			}
-			name = fmt.Sprint(event.ID) + "_pregame_form.json"
-			preFilePath := filepath.Join(filePath, name)
-			writeJson(pre, preFilePath)
+				// pregame form
+				fmt.Println("\tCrawl match", event.Slug, "pregame form")
+				pre, err := server.GetPregameForm(event.ID)
+				if err != nil {
+					log.Println(err)
+				}
+				name = fmt.Sprint(event.ID) + "_pregame_form.json"
+				preFilePath := filepath.Join(filePath, name)
+				writeJson(pre, preFilePath)
+			}(event)
 		}
 	}
+	wg.Wait()
 
 	color.Blue("Crawl team information")
 	for _, team := range table.Standings[0].Rows {
