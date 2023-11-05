5479ae0a212a8ec8c43c197198f1d758df45d4c4
betty2310 <huynh.dh2310@gmail.com>
2023-11-05 23:12:32 +0700

    improve log ouput

--- api/server/event.go
+++ api/server/event.go
@@ -6,21 +6,19 @@ import (
 	"net/http"
 
 	"betty/fb88/api/types"
+
+	"github.com/fatih/color"
 )
 
 func GetMatches(date string) (matches types.Events, error error) {
 	url := fmt.Sprintf("https://api.sofascore.com/api/v1/sport/football/scheduled-events/%s", date)
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
+		color.Red("Error fetching: %v\n\nMaybe you get banned :v", err)
 	}
 	err = json.NewDecoder(response.Body).Decode(&matches)
 	if err != nil {
-		fmt.Printf("error decoding sakura response: %v", err)
-		if e, ok := err.(*json.SyntaxError); ok {
-			fmt.Printf("syntax error at byte offset %d", e.Offset)
-		}
-		fmt.Printf("sakura response: %q", response.Status)
+		color.Red("Error fetching: %v\n\nMaybe you get banned :v", err)
 	}
 	return matches, err
 }
--- api/server/table.go
+++ api/server/table.go
@@ -5,13 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/fatih/color"
 )
 
 func GetTable() (table types.TableData, error error) {
 	url := "https://api.sofascore.com/api/v1/tournament/1/season/52186/standings/total"
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
+		color.Red("Error fetching: %v\n\nMaybe you get banned :v", err)
 	}
 
 	err = json.NewDecoder(response.Body).Decode(&table)
